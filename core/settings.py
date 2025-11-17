77ee1222c1d7372308459a0565becaf672e17694
xonDev-m <muhammadhonasrorov3@gmail.com>
2025-11-17 12:01:59 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['team-r.onrender.com', 'www.team-r.onrender.com', '127.0.0.1']
 
 
 # Application definition
